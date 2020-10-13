78212094d381babe105c031722a16142dfc932a1
alebusi <allebusi@gmail.com>
2020-10-13 23:15:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var testo="";
 $(document).ready(function(){
     $("#testoCompleto").load("https://docs.google.com/document/d/e/2PACX-1vRDihSpthOhnhcIbIi7Z1OfArt2KtozYcXuglANaXJk-xDDWgN9AXM6go_HFLoCaufZjWZV5EEKpq_P/pub span");
 });
-  
+
+/*
 testoRaw=document.getElementById("testoCompleto").innerHTML;
 document.getElementById("testo").innerHTML = testoRaw;
+*/

bb2881477c9d7584becb1d7f7e7e5393cc531b4e
alebusi <allebusi@gmail.com>
2020-10-14 09:11:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var testo="";
-
+/*
 $(document).ready(function(){
     $("#testoCompleto").load("https://docs.google.com/document/d/e/2PACX-1vRDihSpthOhnhcIbIi7Z1OfArt2KtozYcXuglANaXJk-xDDWgN9AXM6go_HFLoCaufZjWZV5EEKpq_P/pub span");
 });
+*/
+$.get("https://docs.google.com/document/d/e/2PACX-1vRDihSpthOhnhcIbIi7Z1OfArt2KtozYcXuglANaXJk-xDDWgN9AXM6go_HFLoCaufZjWZV5EEKpq_P/pub", function(data, status){
+    document.getElementById("testoCompleto").innerHTML = data;
+  });
 
 /*
 testoRaw=document.getElementById("testoCompleto").innerHTML;
